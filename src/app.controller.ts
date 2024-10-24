6965ca3deb8cc9692e6504cf2eea13ab5c5a99a2
Diego Ferrando <dferrando@tecnosoftware.com>
2024-10-24 12:13:48 -0300

    Removed unused functions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,8 @@
-import { Controller, UseGuards, UseInterceptors } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { AuthGuard } from './common/guards/auth.guard';
-import { UserDataInterceptor } from './common/interceptors/userData.interceptor';
-import * as jwt from 'jsonwebtoken';
-import { ROLE_ADMIN, ROLE_GENERAL } from './common/constants/roles';
-import { Permissions } from './common/permissions/permissions';
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-
-  private verifyToken(token: string) {
-    try {
-      const decoded = jwt.decode(token)
-      return decoded; // Return the decoded token if valid
-    } catch (error) {
-      throw new Error('Invalid or expired token');
-    }
-  }
 
   @MessagePattern('checkHealth')
   handleCheckHealth() {
@@ -25,28 +11,5 @@ export class AppController {
     } catch (error) {
       return error;
     }    
-  }
-
-  @UseGuards(AuthGuard)
-  @UseInterceptors(UserDataInterceptor)
-  @Permissions(ROLE_ADMIN, ROLE_GENERAL)
-  @MessagePattern('getPrivate')
-  handleGetPrivate(@Payload() payload: { token: string, message: string, email: string }) {
-    const { token, message, email } = payload;
-    try {
-      // Verify token
-      const decodedToken = this.verifyToken(token.replace('Bearer', '').trim());
-      
-      return {
-        error: 0,
-        message: 'This is a private route. Email: ' + email + ' DecodedToken: ' + JSON.stringify(decodedToken)
-      };
-    } catch (error) {
-      // Return an error response as an object, to keep consistency
-      return { 
-        error: 1,
-        message: error.message 
-      };
-    }
   }
 }
