f6435a89d9335ac82cef8c5edf8f2c21b482bb0e
Diego Ferrando <dferrando@tecnosoftware.com>
2024-10-24 12:13:31 -0300

    Removed unused files

--- src/common/permissions/permissions.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-// Roles list: general, write, admin
-export const Permissions = (...permissions: string[]) =>
-  SetMetadata('permissions', permissions);
